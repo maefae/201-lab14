c1b1dc2a41d8c97fd221ceafd7124e14f678ccd1
mahad Ali <mmahad865@gmail.com>
2022-09-01 16:00:13 -0700

    saving progress

--- js/cart.js
+++ js/cart.js
@@ -19,7 +19,10 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  document.querySelector('tbody').innerHTML = '';
+
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
